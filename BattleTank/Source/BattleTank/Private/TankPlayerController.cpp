687cfd8adb0f4b2cad1e88459e7a722e70ec24e8
Rick Folea <rfolea2@gmail.com>
2016-07-23 16:07:07 -0400

    S4_L118 - Added Tick() Method to player controller

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,10 +3,14 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+// Tick
+// Super
+// AimTowardsCrosshair
+
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
 	auto ControlledTank = GetControlledTank();
 	if (!ControlledTank)
 	{
@@ -16,12 +20,41 @@ void ATankPlayerController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
 	}
+
+	/*
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not Possessing"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *(ControlledTank->GetName()));
+	}  */
+
 //	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+//	UE_LOG(LogTemp, Warning, TEXT("TankPlayerController Tick Method"));
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 	{
 	return Cast<ATank>(GetPawn());
 	}
 
-
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		// Get world location through crosshair
+		// If it hits something (ie landscape) 
+			//Tell controlled tank to aim at this point. 
+	}
+	return;
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -18,4 +18,10 @@ public:
 	void BeginPlay() override;  // override checks to make sure you are using the right function from higher up in the heirarchy.
 								// begin play is in AActor ...
 								// In Actor, BeginPlay was written as a Virtual function which says it can be overwritten by any ansestor
+
+	virtual void Tick(float DeltaTime) override;  // virtual is optional - says if we made a child of this onew any child could use it.
+													// override makes sure there IS a overridable routine in the parent class
+
+	// Start teh tank moving the barrel so that a shot would hit where the sroosshair intersects the world.
+	void AimTowardsCrosshair();
 };
